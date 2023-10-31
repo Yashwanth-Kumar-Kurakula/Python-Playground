8038443cdd9351b05e03519dfba4c1667067eba7
Yashwanth Kumar <62026836+Yashwanth-Kumar-Kurakula@users.noreply.github.com>
2023-10-31 23:35:20 +0530

    Update guess_the_number.py with better code and logic
    * Added Difficulty level to the code
    * Added Input Validation to the code

--- guess-the-number/guess_the_number.py
+++ guess-the-number/guess_the_number.py
@@ -1,52 +1,56 @@
 import random
 
-def guess_the_number(min_value, max_value):
-    """
-    Play the "Guess the Number" game.
+def welcome_message():
+    """Display a welcome message to the user."""
+    print("Welcome to the number guessing game:")
 
-    Args:
-        min_value (int): The minimum number in the range (inclusive).
-        max_value (int): The maximum number in the range (inclusive).
-
-    Returns:
-        None
-    """
-    # Generate a random number within the specified range
-    secret_number = random.randint(min_value, max_value)
-    
-    # Initialize variables to keep track of the number of attempts
-    attempts = 0
-
-    print(f"Welcome to the Guess the Number game! I'm thinking of a number between {min_value} and {max_value}.")
-    
+def select_difficulty():
+    """Prompt the user to select a game difficulty and return the number of tries."""
     while True:
-        # Get the player's guess
         try:
-            player_guess = int(input("Enter your guess: "))
+            difficulty = int(input("Press 1 for Easy Mode\nPress 2 for Normal Mode\nPress 3 for Hard Mode\nEnter the difficulty that you want to play: "))
+            if 1 <= difficulty <= 3:
+                return [10, 5, 3][difficulty - 1]
+            else:
+                print("Invalid input. Please enter 1, 2, or 3.")
         except ValueError:
-            print("Please enter a valid number.")
-            continue
+            print("Invalid input. Please enter 1, 2, or 3.")
 
-        attempts += 1
-
-        if player_guess < min_value or player_guess > max_value:
-            print(f"Please guess a number between {min_value} and {max_value}.")
-        elif player_guess < secret_number:
-            print("Try a higher number.")
-        elif player_guess > secret_number:
-            print("Try a lower number.")
-        else:
-            print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
-            break
+def get_valid_guess():
+    """Get a valid numeric guess from the user."""
+    while True:
+        try:
+            guess = int(input("Enter a number between 1 and 100: "))
+            if 1 <= guess <= 100:
+                return guess
+            else:
+                print("Invalid input. Please enter a number between 1 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 100.")
 
 def main():
-    """
-    Main function to start the game.
-    """
-    min_value = 1
-    max_value = 100  # You can adjust the range as needed.
+    """Main function to run the number guessing game."""
+    welcome_message()
+    
+    game_on = True
+    correct_number = random.randint(1, 100)
+    
+    tries = select_difficulty()
+    
+    while game_on and tries > 0:
+        guess = get_valid_guess()
+        if guess == correct_number:
+            print("You have guessed correctly!")
+            game_on = False
+        elif guess > correct_number:
+            print("You need to guess a lower number")
+        else:
+            print("You need to guess a higher number")
+        
+        tries -= 1
 
-    guess_the_number(min_value, max_value)
+    if tries == 0:
+        print(f"Oops! You are out of tries, the correct number was {correct_number}")
 
-
-main()
+if __name__ == "__main__":
+    main()
