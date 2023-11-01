2bab216f9dd359c9348bbb50af1686b402f9994c
Timothée Marrero Meyer <143833750+timotheeMM@users.noreply.github.com>
2023-11-01 15:58:42 +0100

    Hangman game: add the words to the words.py file

--- hangman/words.py
+++ hangman/words.py
@@ -1,1 +1,15 @@
-words = []
+words = [
+    "computer", "language", "programming", "algorithm", "tablet", "phone",
+    "house", "animal", "plant", "color", "country", "city", "mountain", "ocean",
+    "insect", "bird", "fish", "fruit", "vegetable", "clothing", "utensil", "tool",
+    "machine", "sport", "music", "science", "history", "geography", "mathematics",
+    "physics", "chemistry", "biology", "politics", "economics", "society", "culture",
+    "love", "hate", "sadness", "anger", "fear", "illness", "health", "small",
+    "long", "short", "fast", "slow", "left", "right", "elephant", "giraffe", "lion",
+    "monkey", "panda", "rhinoceros", "tiger", "whale", "bear", "horse", "sheep",
+    "reptile", "amphibian", "apple", "banana", "bread", "cake", "chicken", "chocolate",
+    "ice cream", "pizza", "potato", "rice", "salad", "sandwich", "soup", "steak",
+    "airport", "forest", "park", "river", "school", "store", "bicycle", "book", "chair",
+    "lamp", "shirt", "dance", "sleep", "swim", "walk", "write", "happy", "angry", "scared",
+    "surprised", "tired", "galaxy", "universe"
+]
