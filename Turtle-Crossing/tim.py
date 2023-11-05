4e09000bab524ba6d2ac3f5f6d5b1e60d1d7fef5
Yashwanth Kumar <62026836+Yashwanth-Kumar-Kurakula@users.noreply.github.com>
2023-11-05 23:40:30 +0530

    Added Docstrings
    Added Docstrings

--- Turtle-Crossing/tim.py
+++ Turtle-Crossing/tim.py
@@ -1,19 +1,30 @@
 from turtle import Turtle
 
 class Tim(Turtle):
+    """
+    A class representing the player character 'Tim' in a Turtle graphics game.
+    Tim is a turtle that can move forward and backward on the screen.
+    """
     def __init__(self):
+        """
+        Initializes a Tim object with default attributes and initial position.
+        """
         super().__init__()
         self.shape("turtle")
         self.color("black")
         self.penup()
-        self.speed = 10
-        self.setheading(90)
-        self.goto(0, -280)
-
+        self.speed = 10  # The speed at which Tim moves.
+        self.setheading(90)  # Set Tim's initial orientation (facing upward).
+        self.goto(0, -280)  # Set Tim's initial position at the bottom of the screen.
 
     def move_forward(self):
+        """
+        Move Tim forward by a distance determined by the 'speed' attribute.
+        """
         self.forward(self.speed)
 
-
     def move_backward(self):
+        """
+        Move Tim backward by a distance determined by the 'speed' attribute.
+        """
         self.backward(self.speed)
