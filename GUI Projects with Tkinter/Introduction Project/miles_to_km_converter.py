e2ebd8eeae1d6f123db2540903cf36642b5290ef
Yashwanth Kumar <62026836+Yashwanth-Kumar-Kurakula@users.noreply.github.com>
2023-11-29 22:44:00 +0530

    Added docstrings and comments wherever necessary
    Added docstrings and comments wherever necessary

--- GUI Projects with Tkinter/Introduction Project/miles_to_km_converter.py
+++ GUI Projects with Tkinter/Introduction Project/miles_to_km_converter.py
@@ -2,28 +2,48 @@ import tkinter as tk
 import ttkbootstrap as ttk
 
 def convert():
+    """
+    Converts miles to kilometers and updates the output label.
+    """
+    # Get the mile input from the entry widget
     mile_input = entry_int.get()
+
+    # Convert miles to kilometers and round to 2 decimal places
     km_output = round(mile_input * 1.61, 2)
+
+    # Update the output label with the result
     output_string.set(km_output)
 
-window = ttk.Window(themename= 'simplex')
+# Create a Tkinter window using ttkbootstrap theme
+window = ttk.Window(themename='simplex')
 window.title("Demo")
 window.geometry('300x150')
 
-title_label = ttk.Label(master = window, text = "Miles to kilometers", font = "Calibri 24 bold")
+# Create and pack the title label
+title_label = ttk.Label(master=window, text="Miles to kilometers", font="Calibri 24 bold")
 title_label.pack()
 
+# Create an input frame to hold the entry widget and button
 input_frame = ttk.Frame(window)
-entry_int = tk.IntVar()
-entry = ttk.Entry(input_frame, textvariable= entry_int)
-button = ttk.Button(input_frame, text="Convert", command=convert)
 
-entry.pack(side = "left", padx = 10)
+# Variable to store the mile input from the entry widget
+entry_int = tk.IntVar()
+
+# Create entry widget and button, and pack them into the input frame
+entry = ttk.Entry(input_frame, textvariable=entry_int)
+button = ttk.Button(input_frame, text="Convert", command=convert)
+entry.pack(side="left", padx=10)
 button.pack()
+
+# Pack the input frame into the window with some padding
 input_frame.pack(pady=10)
 
+# Variable to store the converted kilometers for output label
 output_string = tk.StringVar()
-output_label = ttk.Label(window, text="Output", textvariable=output_string, font = "Calibri 24 bold")
+
+# Create and pack the output label
+output_label = ttk.Label(window, text="Output", textvariable=output_string, font="Calibri 24 bold")
 output_label.pack(pady=10)
 
+# Start the Tkinter event loop
 window.mainloop()
