b6b7602cae480c3731d24f2e83e653519642b575
Yashwanth Kumar <62026836+Yashwanth-Kumar-Kurakula@users.noreply.github.com>
2023-12-09 23:46:04 +0530

    Added docstrings and comments wherever necessary
    Added docstrings and comments wherever necessary

--- GUI Projects with Tkinter/Introduction Project/2. Simple Paint using Canvas/simple_paint_using_canvas.py
+++ GUI Projects with Tkinter/Introduction Project/2. Simple Paint using Canvas/simple_paint_using_canvas.py
@@ -2,24 +2,39 @@ import tkinter as tk
 
 window = tk.Tk()
 
-canvas = tk.Canvas(window, bg= "white", width=800, height=800)
+canvas = tk.Canvas(window, bg="white", width=800, height=800)
 canvas.pack()
 
 def draw_on_canvas(event):
+    """
+    Draw an oval on the canvas at the specified event coordinates.
+    
+    Parameters:
+        - event (tk.Event): Mouse event containing information about the cursor position.
+    """
     x = event.x
     y = event.y
-    canvas.create_oval(x - brush_size / 2, y - brush_size ,x + brush_size / 2 ,y + brush_size/2, fill="black")
+    canvas.create_oval(x - brush_size / 2, y - brush_size, x + brush_size / 2, y + brush_size / 2, fill="black")
 
 def brush_size_adjust(event):
+    """
+    Adjust the brush size based on the mouse wheel movement.
+    
+    Parameters:
+        - event (tk.Event): Mouse wheel event containing information about the wheel movement.
+    """
     global brush_size
     if event.delta > 0:
         brush_size += 2
     else:
         brush_size -= 2
     
-    brush_size = max(0,min(brush_size, 50))
+    brush_size = max(0, min(brush_size, 50))
 
+# Initial brush size
 brush_size = 2
+
+# Bind functions to canvas events
 canvas.bind("<B1-Motion>", draw_on_canvas)
 canvas.bind("<MouseWheel>", brush_size_adjust)
 
