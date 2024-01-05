a4c88dcf5a7f4b6701db9fb5bdb075617d713a5d
Timothée MARRERO MEYER <143833750+timotheemm@users.noreply.github.com>
2024-01-05 12:25:45 +0100

    Initialize the window and canvas

---
+++ Submarine-Game/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+HEIGHT = 500
+WIDTH = 800
+
+window = Tk()
+window.title('Submarine Game')
+
+canvas = Canvas(window, height=HEIGHT, width=WIDTH, bg='darkblue')
+canvas.pack()
