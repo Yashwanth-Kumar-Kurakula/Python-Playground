d92ae014161c8a04ef892f37f9f12b2d98524170
Timothée MARRERO MEYER <143833750+timotheemm@users.noreply.github.com>
2024-01-05 12:30:18 +0100

    Initialize the submarine

--- Submarine-Game/main.py
+++ Submarine-Game/main.py
@@ -8,3 +8,14 @@ window.title('Submarine Game')
 
 canvas = Canvas(window, height=HEIGHT, width=WIDTH, bg='darkblue')
 canvas.pack()
+
+submarine_id = canvas.create_polygon(5, 5, 5, 25, 30, 15, fill='red')
+submarine2_id = canvas.create_oval(0, 0, 30, 30, outline='red')
+
+SUBMARINE_RADIUS = 15
+
+X_CENTER = WIDTH / 2
+Y_CENTER = HEIGHT / 2
+
+canvas.move(submarine_id, X_CENTER, Y_CENTER)
+canvas.move(submarine2_id, X_CENTER, Y_CENTER)
