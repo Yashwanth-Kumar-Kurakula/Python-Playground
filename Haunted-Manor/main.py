774049f841bc697f8102fcad1dbf8656f65bef30
Timothée MARRERO MEYER <143833750+timotheemm@users.noreply.github.com>
2024-01-07 18:09:17 +0100

    Initialize the main loop

--- Haunted-Manor/main.py
+++ Haunted-Manor/main.py
@@ -3,3 +3,13 @@ from random import randint
 print('THE HAUNTED MANOR')
 i_am_brave = True
 score = 0
+
+# main loop
+
+while i_am_brave:
+    ghost_door = randint(1, 3)
+    print('You find yourself facing three doors...')
+    print('Behind which is the ghost?')
+    print('What door do you open?')
+    door = input('1, 2 or 3? ')
+    door_number = int(door)
