7c398fe730ca080cdd3052ffe83549878942bc84
Timothée MARRERO MEYER <143833750+timotheemm@users.noreply.github.com>
2024-01-08 17:03:54 +0100

    Manage the case where the user do not enter a valid input

--- Haunted-Manor/main.py
+++ Haunted-Manor/main.py
@@ -10,17 +10,27 @@ while i_am_brave:
     print('You find yourself facing three doors...')
     print('Behind which is the ghost?')
     print('What door do you open?')
-    
-    door = input('1, 2 or 3? ')
-    door_number = int(door)
 
-    if door_number == ghost_door:
-        print('\nA GHOST!')
-        i_am_brave = False
-    else:
-        print('\nNo ghost!')
-        print('You enter the next room.')
-        score += 1
+    door = input('1, 2 or 3? ')
+    
+    try:
+        door_number = int(door)
+        
+        if door_number < 1 or door_number > 3:
+            print('\nPlease enter a valid door number (1, 2, or 3).')
+            continue  # Ask for input again in case of an invalid door number
+
+        if door_number == ghost_door:
+            print('\nA GHOST!')
+            i_am_brave = False
+
+        else:
+            print('\nNo ghost!')
+            print('You enter the next room.')
+            score += 1
+            
+    except ValueError:
+        print('\nPlease enter a valid integer.')
 
 print('Help!')
 print('Game over! Your score:', score)
