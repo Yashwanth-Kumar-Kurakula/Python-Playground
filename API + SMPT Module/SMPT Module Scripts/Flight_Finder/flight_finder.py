7872acb44801b312e579a730ca1c3ca731d65111
Yashwanth Kumar <itsmectm@gmail.com>
2024-01-10 23:43:29 +0530

    add docstrings and comments wherever necessary

--- API + SMPT Module/SMPT Module Scripts/Flight_Finder/flight_finder.py
+++ API + SMPT Module/SMPT Module Scripts/Flight_Finder/flight_finder.py
@@ -1,22 +1,37 @@
 import requests
 import json
 
-FLY_FROM = "HYD" #Change this field to IATA code of place you want to fly from
-FLY_TO = "MEL" #Change this field to IATA code of place you want to fly 
+FLY_FROM = "HYD"  # Change this field to IATA code of the place you want to fly from
+FLY_TO = "MEL"    # Change this field to IATA code of the place you want to fly
 
 def dt_formatter(dt):
+    """
+    Formats date and time.
+
+    Args:
+        dt (str): Input date and time string.
+
+    Returns:
+        str: Formatted date and time string.
+    """
     date = dt[0:10]
     time = dt[11:16]
     return f"{date} at {time}"
 
 class FlightSearch:
     def __init__(self):
-        self.API_KEY="" #Enter Your API Key Here
-        self.url="https://api.tequila.kiwi.com/locations/query"
+        self.API_KEY = ""  # Enter Your API Key Here
+        self.url = "https://api.tequila.kiwi.com/locations/query"
 
     def get_price(self):
+        """
+        Fetches flight prices using the Tequila API.
+
+        Returns:
+            dict: JSON response containing flight data.
+        """
         search_url = "https://api.tequila.kiwi.com/v2/search"
-        header = { 
+        header = {
             "apikey": self.API_KEY,
             "Accept": "application/json"
         }
@@ -28,24 +43,26 @@ class FlightSearch:
             "max_fly_duration": "18",
             "adults": "1",
             "selected_cabins": "M",
-            # "adult_hold_bag": "2",
-            # "adult_hand_bag": "1",
             "max_stopovers": "1",
             "curr": "INR",
         }
         response = requests.get(url=search_url, headers=header, params=parameter)
         temp = response.json()
         return temp
-    
+
+# Instantiate FlightSearch class
 fs = FlightSearch()
 data = fs.get_price()
 
+# Save the JSON data to a file
 with open("scrap.json", "w") as file:
     json.dump(data, file)
 
+# Load airline codes from a file
 with open("airline_codes.json", "r") as file:
     airline_codes = json.load(file)
 
+# Display flight information
 for j in range(10):
     price = data["data"][j]["price"]
     hand_bag_limit = data["data"][j]["baglimit"]["hand_weight"]
@@ -55,7 +72,7 @@ for j in range(10):
 
     route = [data["data"][0]["route"][i] for i in range(len(data["data"][0]["route"]))]
 
-    print(f"FLIGHT NUMBER : {j+1}\nFROM: HYD\nTO: MEL\nPRICE: {price}")
+    print(f"FLIGHT NUMBER : {j+1}\nFROM: {FLY_FROM}\nTO: {FLY_TO}\nPRICE: {price}")
     print(f"HAND BAGGAGE LIMIT (IN KGS): {hand_bag_limit}")
     print(f"CHECK-IN BAGGAGE LIMIT (IN KGS): {luggage_limit}")
 
@@ -76,14 +93,7 @@ for j in range(10):
         print(f"LOCAL DEPARTURE TIMINGS: {local_departure_timings}")
         local_arrival_timings = dt_formatter(route[i]["local_arrival"])
         print(f"LOCAL ARRIVAL TIMINGS: {local_arrival_timings} \n")
-    
+
     print(f"For more information, follow this link: {link}")
-    
+
     print("_____________________________________________________")
-    
-
-
-
-
-
-
